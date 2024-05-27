2eb9f592d1c352340bdca41e9ca2e93e1e938956
Hyunmin <momomomkiop3@gmail.com>
2024-05-27 11:48:58 +0900

    picturebox 테스트

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(-3, -48);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(392, 841);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(384, 761);
+            Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
 namespace ShootingGame
 {
     public partial class Form1 : Form
@@ -5,7 +10,27 @@ namespace ShootingGame
         public Form1()
         {
             InitializeComponent();
-            
+            LoadImage();
+
+            void LoadImage()
+            {
+                try
+                {
+                    // 이미지 경로 설정 (절대 경로 또는 상대 경로)
+                    string imagePath = "D:\\work\\ShootingGame\\images\\배경.png";
+
+                    // 이미지 로드
+                    pictureBox1.Image = Image.FromFile(imagePath);
+
+                    // 이미지 크기를 PictureBox에 맞게 조정
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                catch (Exception ex)
+                {
+                    // 이미지 로드 실패 시 예외 처리
+                    MessageBox.Show("이미지를 로드하는 동안 오류가 발생했습니다: " + ex.Message);
+                }
+            }
         }
 
         public void test()
